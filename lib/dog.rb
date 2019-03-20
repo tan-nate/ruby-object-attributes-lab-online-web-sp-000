9798621494b6f075aba2691ea780fa193a565b3e
Nate Tan <nktan93@gmail.com>
2019-03-20 21:48:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name
+    @name
   end
  
-  def name
-    this_dogs_name
+  def name=(new_name)
+    @name = new_name
   end
   
   
